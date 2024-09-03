d98bb29f78abfa77eb757aa05103bcef80b61a6e
ButerbrodLZ <mark.chernous@bk.ru>
2024-09-03 15:42:40 +0700

    Create getammo.lua

---
+++ getammo.lua
@@ -0,0 +1,20 @@
+PLUGIN.name = "getammo"
+PLUGIN.author = "Buterbrod"
+PLUGIN.description = "maybe good for ivent-masters"
+
+
+    local ammoTypes = {
+   --[  ["5.45"] = "ammo_5_45"
+  
+    }
+
+
+    ix.command.Add("getammo", {
+        description = "getting ammos",
+        adminOnly = true, 
+        OnRun = function(self, client)
+            for ammoName, ammoType in pairs(ammoTypes) do
+                client:GiveAmmo(500, ammoType)
+            end
+        end
+    })
